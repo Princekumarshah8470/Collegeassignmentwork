78469b5d0458d931430ce5534a95f5b1505d818c
Princekumarshah8470 <25113cn193@glbitm.ac.in>
2026-06-14 21:30:58 +0530

    Add files via upload

---
+++ Day12/Problem46.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int isArmstrong(int n) {
+    int original = n, remainder, digits = 0;
+    int sum = 0;
+
+    // Count digits
+    int temp = n;
+    while (temp != 0) {
+        digits++;
+        temp /= 10;
+    }
+
+    temp = n;
+
+    while (temp != 0) {
+        remainder = temp % 10;
+        sum += pow(remainder, digits);
+        temp /= 10;
+    }
+
+    return (sum == original);
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isArmstrong(num))
+        printf("%d is an Armstrong Number.\n", num);
+    else
+        printf("%d is not an Armstrong Number.\n", num);
+
+    return 0;
+}
